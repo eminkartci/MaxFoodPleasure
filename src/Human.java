5e0599ae82b7ae7b301140e3b34ec15cb263bfd6
eminkartci <emin.kartci@ozu.edu.tr>
2021-04-29 22:13:33 +0300

    Human class is implemented

--- src/Human.java
+++ src/Human.java
@@ -1,4 +1,16 @@
 
 public class Human {
 
+	// Attributes
+	private String 	name;
+    private int 	money;
+    private int 	foodCapacity;
+    private int 	pleasure;
+
+    public Human(String name) {
+        this.name = name;
+    }
+
+
+
 }
