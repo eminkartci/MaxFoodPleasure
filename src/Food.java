4914671e39d25c3152033f66c549faa741a18301
eminkartci <emin.kartci@ozu.edu.tr>
2021-05-02 14:31:25 +0300

    Food class is implemented

---
+++ src/Food.java
@@ -0,0 +1,21 @@
+
+
+public class Food {
+
+    // Attributes
+    private String name;
+    private int cost;
+    private int satiety;    // Doyuruculuk
+    private int pleasure;
+
+
+    // Constructor
+    public Food(String name,int cost,int satiety,int pleasure){
+        this.name = name;
+        this.cost = cost;
+        this.satiety = satiety;
+        this.pleasure = pleasure;
+    }
+
+    
+}
